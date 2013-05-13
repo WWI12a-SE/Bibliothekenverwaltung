737e3bd2bd62ce76468bae972ad395e6a313e23a
weisseth <weisseth@dhbw-loerrach.de>
2013-05-13 10:57:00 +0200

    JavaDoc fuer User und UserHandler

--- controller/UserHandler.java
+++ controller/UserHandler.java
@@ -9,6 +9,18 @@ package controller;
 import core.CsvHandler;
 import model.*;
 
+/**
+ * <p>
+ * Die Klasse UserHandler verwaltet alle User-Objekte.
+ * Sie dient der Benutzeroberfläche als Schnittstelle um einzelne oder alle User abzufragen,
+ * d.h. um Referenzen auf User-Objekte übergeben zu bekommen.
+ * </p><p>
+ * Der Konstruktor dieser Klasse ist, um eine mehrmalige Instanzierung zu verhindern, privatisiert.
+ * Um ein Objekt des UserHandlers zu erhalten muss die statische getInstance-Methode verwendet werden.
+ * </p>
+ * @author weisseth
+ * @package controller
+ */
 public class UserHandler {
 
 	private static final String S_FILE_NAME = "users";
@@ -16,11 +28,24 @@ public class UserHandler {
 	private CsvHandler csvHandler;
 	private User[] users; // Alle Benutzer als Objekt in dieses Array
 	
+	/**
+	 * Der Konstruktor dieser Klasse ist, um eine mehrmalige Instanzierung zu verhindern, privatisiert.
+	 * Um ein Objekt des UserHandlers zu erhalten muss die statische getInstance-Methode verwendet werden.
+	 */
 	private UserHandler()
 	{
 		csvHandler = new CsvHandler(S_FILE_NAME);
 	}
 	
+	/**
+	 * <p>
+	 * Die statische getInstance-Methode dient als Fabrikmethode:
+	 * </p><p>
+	 * Sie initialisiert einmalig ein UserHandler-Objekt.
+	 * Jeder weitere Methodenaufruf übergibt eine Referenz auf das gleiche UserHandler-Objekt.
+	 * </p>
+	 * @return userHandlerObject : UserHandler -- Objekt des UserHandlers
+	 */
 	public static UserHandler getInstance(){
 		if(userController == null){
 			userController = new UserHandler();
@@ -28,6 +53,16 @@ public class UserHandler {
 		return userController;
 	}
 	
+	/**
+	 * <p>
+	 * Die Methode getAllUsers() holt sich alle User-Daten aus der entsprechenden CSV-Datei.
+	 * Fuer jeden User wird ein neues User-Objekt instanziiert und als Array zurueck gegeben.
+	 * </p><p>
+	 * Zuvor initialisierte User werden ebenfalls neu geladen.
+	 * Aenderungen welche nicht explizit ueber den CSV-Handler gespeichert wurden gehen hierbei verloren.
+	 * </p>
+	 * @return allUsers : User[] -- Array aller User-Objekte
+	 */
 	public User[] getAllUsers()
 	{
 		String[][] userMap = csvHandler.read();
@@ -38,21 +73,29 @@ public class UserHandler {
 		return users;
 	}
 	
+	/**
+	 * <p>
+	 * Die Methode getUser gibt ein Objekt des gesuchten Users zurueck.
+	 * Die Suche muss ueber den LoginNamen als eindeutige ID spezifiziert werden.
+	 * Sollte der LoginName nicht vergeben sein wird ein neues User-Objekt erstellt und uebergeben.
+	 * </p>
+	 * @param loginName : String -- Die ID des gesuchten Users
+	 * @return user : User -- Objekt des gesuchten Users
+	 */
 	public User getUser(String loginName)
 	{
 		int newIndex;
-		User[] oldUsers;
 		
 		//Durchsuche vorhandene User
 		if(users != null){
 			for(int i = 0; i < users.length; i++){
 				if(users[i].getLoginName().equals(loginName)){
-					return users[i];
+					return users[i]; //User-Objekt gefunden
 				}
 			}
 			newIndex = users.length;
 			//Merke schon initialisierte User
-			oldUsers = users;
+			User[] oldUsers = users;
 			//Erweitere User-Array
 			users = new User[newIndex+1];
 			//Kopieren des alten User-Arrays	
@@ -72,6 +115,11 @@ public class UserHandler {
 		return users[newIndex];
 	}
 	
+	/**
+	 * Die Methode save() weist den CsvHandler des UserHandlers an die "staged"-ten
+	 * (zum speichern bereiten) Aenderungen in die CSV-Datei zu uebertragen. Alle Aenderungen
+	 * an User-Objekten werden automatisch ge-"staged".
+	 */
 	public void save(){
 		csvHandler.save();
 	}
--- model/User.java
+++ model/User.java
@@ -8,6 +8,17 @@
 package model;
 import core.*;;
 
+/**
+ * <p>
+ * Die Klasse User repraesentiert einen Benutzer und stellt die in der CSV-Datei 
+ * gespeicherten Daten zur Verfuegung.
+ * </p><p>
+ * Um einen User zu instanziieren muss ueber ein UserHandler-Objekt die Methode 
+ * getUser() oder getAllUser() verwendet werden um die Konsistenz der Daten zu gewaehrleisten.
+ * </p>
+ * @author weisseth
+ * @package model
+ */
 public class User {
 	
 	//CSV-Spalten
@@ -32,54 +43,115 @@ public class User {
 	private int iRole = 0; // Benutzerrolle, siehe Konstanten
 	private CsvHandler csvHandler;
 	
+	/**
+	 * Getter des LoginNamens, der ID des Users
+	 * @return loginName : String
+	 */
 	public String getLoginName() {
 		return sLoginName;
 	}
-	public void setLoginName(String sLoginName) {
-		if(!this.sLoginName.equals(sLoginName)){
-			this.sLoginName = sLoginName;
+	
+	/**
+	 * Setter des LoginNamens, der ID des Users
+	 * @param loginName : String
+	 */
+	public void setLoginName(String loginName) {
+		if(!this.sLoginName.equals(loginName)){
+			this.sLoginName = loginName;
 			this.stage();
 		}
 	}
+	
+	/**
+	 * Getter des Vornamens
+	 * @return sFirstName : String
+	 */
 	public String getFirstName() {
 		return sFirstName;
 	}
+	
+	/**
+	 * Setter des Vornamens
+	 * @param sFirstName : String
+	 */
 	public void setFirstName(String sFirstName) {
 		if(!this.sFirstName.equals(sFirstName)){
 			this.sFirstName = sFirstName;
 			this.stage();
 		}
 	}
+	
+	/**
+	 * Getter des Nachnamens
+	 * @return sLastName : String
+	 */
 	public String getLastName() {
 		return sLastName;
 	}
+	
+	/**
+	 * Setter des Nachnamens
+	 * @param sLastName : String
+	 */
 	public void setLastName(String sLastName) {
 		if(!this.sLastName.equals(sLastName)){
 			this.sLastName = sLastName;
 			this.stage();
 		}
 	}
+	
+	/**
+	 * Getter der BenutzerRolle.
+	 * Die Bedeutung des Wertes ist den User-Konstanten zu entnehmen.
+	 * @return iRole : Integer
+	 */
 	public int getRole() {
 		return iRole;
 	}
+	
+	/**
+	 * Setter der BenutzerRolle.
+	 * Die Bedeutung des Wertes ist den User-Konstanten zu entnehmen.
+	 * @param iRole : Integer
+	 */
 	public void setRole(int iRole) {
 		if(this.iRole != iRole){
 			this.iRole = iRole;
 			this.stage();
 		}
 	}
+	
+	/**
+	 * Getter des Passworts
+	 * @return sPassword : String
+	 */
 	public String getPassword() {
 		return sPassword;
 	}
+	
+	/**
+	 * Setter des Passworts
+	 * @param sPassword : String
+	 */
 	public void setPassword(String sPassword) {
 		if(!this.sPassword.equals(sPassword)){
 			this.sPassword = sPassword;
 			this.stage();
 		}
 	}
+	
+	/**
+	 * Getter der EMail
+	 * @return sEmail : String
+	 */
 	public String getEmail() {
 		return sEmail;
 	}
+	
+	/**
+	 * Setter der EMail
+	 * @param sEmail : String
+	 */
 	public void setEmail(String sEmail) {
 		if(!this.sEmail.equals(sEmail)){
 			this.sEmail = sEmail;
@@ -87,6 +159,17 @@ public class User {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Konstruktor des Users. Stellt die in der CSV-Datei 
+	 * gespeicherten Daten zur Verfuegung.
+	 * </p><p>
+	 * Um einen User zu instanziieren muss ueber ein UserHandler-Objekt die Methode 
+	 * getUser() oder getAllUser() verwendet werden um die Konsistenz der Daten zu gewaehrleisten.
+	 * </p>
+	 * @param csvHandler : CsvHandler
+	 * @param loginName : String
+	 */
 	public User(CsvHandler csvHandler, String loginName){
 		this.csvHandler = csvHandler;
 		String[] values = csvHandler.getLineById(loginName);
@@ -96,9 +179,17 @@ public class User {
 		this.setLastName(values[COL_LASTNAME]);
 		this.setPassword(values[COL_PASSWORD]);
 		this.setRole(Integer.parseInt(values[COL_ROLE]));
+		//Schreibe values fuer den fall dass das Objekt neu ist und der CSV-Handler 
+		//noch keinen Datensatz angelegt hat.
+		this.stage();
 	}
 	
-	public String[] getValuesAsStringArray(){
+	/**
+	 * Liest alle Attribute des Users aus und gibt diese als String-Array zurueck.
+	 * Die Indiziers entsprechen den CSV-Spalten, welche den User-Konstanten zu entnehmen sind.
+	 * @return values : String[]
+	 */
+	private String[] getValuesAsStringArray(){
 		String[] values = new String[AMOUNT_COLUMNS];
 		values[COL_ROLE] = Integer.toString(this.iRole);
 		values[COL_LOGINNAME] = this.sLoginName;
@@ -109,7 +200,11 @@ public class User {
 		return values;
 	}
 	
-	public void stage()
+	/**
+	 * Die Attribute des Users werden im CsvHandler zwischengespeichert und somit 
+	 * zum speichern in der CSV-Datei bereit gestellt ("gestaged").
+	 */
+	private void stage()
 	{
 		csvHandler.update(this.getValuesAsStringArray());
 	}
