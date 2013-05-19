cc5e089a96013c7b00cf763b348f971c653d3f23
weisseth <weisseth@dhbw-loerrach.de>
2013-05-19 17:51:36 +0200

    Konstanten, 2. SubPanel, Rename MediaController => StockLogic

---
+++ controller/StockLogic.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class StockLogic {
+	
+	public void reserve(int mediumID){
+//		ReservationHandler reservationHandler = ReservationHandler.getInstance();
+//		CsvHandler csvHandler = new CsvHandler();
+//		reservationHandler.getReservation(csvHandler.);
+	}
+}
--- view/StockView.java
+++ view/StockView.java
@@ -4,12 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.table.*;
 import model.Medium;
 import model.User;
 import controller.MediaHandler;
+import controller.ReservationHandler;
 
 import javax.swing.event.*;
 import javax.swing.table.TableModel;
@@ -22,10 +26,28 @@ import javax.swing.table.TableModel;
 public class StockView extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private static final int SUBPANEL_EAST_WIDTH = 40;
-	private static final int SUBPANEL_EAST_HEIGHT = 600;
-	private static final int SUBPANEL_NORTH_WIDTH = 600;
-	private static final int SUBPANEL_NORTH_HEIGHT = 40;
+	
+	private static final int SUBPANEL_NORTH_INDEX = 0;
+	private static final int SUBPANEL_EAST_INDEX = 1;
+	private static final int SUBPANEL_SOUTH_INDEX = 2;
+	private static final int SUBPANEL_WEST_INDEX = 3;
+	
+	private static final int SUBPANEL_VERTICAL_WIDTH = 40;
+	private static final int SUBPANEL_VERTICAL_HEIGHT = 600;
+	private static final int SUBPANEL_HORIZONTAL_WIDTH = 600;
+	private static final int SUBPANEL_HORIZONTAL_HEIGHT = 40;
+	private static final int SUBPANEL_SEPERATOR_WIDTH = 20;
+	private static final int SUBPANEL_SEPERATOR_HEIGHT = 600;
+	
+	private static final int COL_TITLE = 0;
+	private static final int COL_AUTHOR = 1;
+	private static final int COL_PUBLISHER = 2;
+	private static final int COL_EDITION = 3;
+	private static final int COL_ISBN = 4;
+	private static final int COL_COPIES = 5;
+	private static final int COL_ONSTOCK = 6;
+	private static final int COL_KEYWORDS = 7;
+	private static final int COL_ACTIONS = 8;
 
 	private JTable stockTable;
 	private StockTableModelListener stockTableModelListener;
@@ -33,6 +55,9 @@ public class StockView extends JPanel {
 	private JScrollPane scrollPane;
 	private JPanel scrollPanePanel;
 	private JPanel[] scrollPaneBorderPanels;
+	
+	private JButton buttonLease, buttonReturn, buttonDelete, buttonNew;
+	
 	private int mode;
 	private int[] IDs;
 
@@ -59,24 +84,72 @@ public class StockView extends JPanel {
 	}
 	
 	private void addBorderPanels(){
-		//Init
+		//Array-Init
 		scrollPaneBorderPanels = new JPanel[4];
+		//All
 		for(int i = 0; i < scrollPaneBorderPanels.length; i++){
+			//Init
 			scrollPaneBorderPanels[i] = new JPanel();
-			//Size
-			if(i % 2 == 0){
-				scrollPaneBorderPanels[i].setPreferredSize(new Dimension(SUBPANEL_NORTH_WIDTH,SUBPANEL_NORTH_HEIGHT));
-			}else{
-				scrollPaneBorderPanels[i].setPreferredSize(new Dimension(SUBPANEL_EAST_WIDTH,SUBPANEL_EAST_HEIGHT));
-			}
 			//Color
 			scrollPaneBorderPanels[i].setBackground(Color.WHITE);
 		}
+		//Size
+		scrollPaneBorderPanels[SUBPANEL_NORTH_INDEX].setPreferredSize(new Dimension(SUBPANEL_HORIZONTAL_WIDTH, SUBPANEL_HORIZONTAL_HEIGHT));
+		scrollPaneBorderPanels[SUBPANEL_EAST_INDEX].setPreferredSize(new Dimension(200, SUBPANEL_VERTICAL_HEIGHT));
+		scrollPaneBorderPanels[SUBPANEL_SOUTH_INDEX].setPreferredSize(new Dimension(SUBPANEL_HORIZONTAL_WIDTH, SUBPANEL_HORIZONTAL_HEIGHT));
+		scrollPaneBorderPanels[SUBPANEL_WEST_INDEX].setPreferredSize(new Dimension(40, SUBPANEL_VERTICAL_HEIGHT));
+		
+		//Inner-Layout
+		scrollPaneBorderPanels[SUBPANEL_EAST_INDEX].setLayout(new BorderLayout());
+		
+		JPanel middleBorderPanel = new JPanel();
+		JPanel eastBorderPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		
+		middleBorderPanel.setBackground(Color.WHITE);
+		eastBorderPanel.setBackground(Color.WHITE);
+		centerPanel.setBackground(Color.WHITE);
+		buttonLease = new JButton("Ausleihen");
+		buttonLease.addActionListener(new ActionListener(){
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if(stockTable.getSelectedRowCount() != 1){//Fehler
+					String message = "";
+					if(stockTable.getSelectedRowCount() == 0){
+						message = "Bitte selektieren Sie das Buch, welches Sie entleihen wollen!";
+					}
+					if(stockTable.getSelectedRowCount() > 1){
+						message = "Bitte selektieren Sie nur ein Buch!";
+					}
+					JOptionPane.showMessageDialog(null, message, "Fehler", JOptionPane.CANCEL_OPTION);
+				}else{//Auswahl OK
+					int selectedIndex = stockTable.getSelectedColumn();
+//					stockLogic.reserve(IDs[selectedIndex]);
+				}
+			}
+			
+		});
+		buttonReturn = new JButton("Zurückgeben");
+		buttonLease.setPreferredSize(new Dimension(126,30));
+		buttonReturn.setPreferredSize(new Dimension(126,30));
+		buttonLease.setMargin(new Insets(0,0,0,0));
+		buttonReturn.setMargin(new Insets(0,0,0,0));
+		centerPanel.add(buttonReturn);
+		centerPanel.add(buttonLease);
+		
+		middleBorderPanel.setPreferredSize(new Dimension(SUBPANEL_SEPERATOR_WIDTH, SUBPANEL_SEPERATOR_HEIGHT));
+		eastBorderPanel.setPreferredSize(new Dimension(SUBPANEL_VERTICAL_WIDTH, SUBPANEL_VERTICAL_HEIGHT));
+		scrollPaneBorderPanels[SUBPANEL_EAST_INDEX].add(middleBorderPanel, BorderLayout.WEST);
+		scrollPaneBorderPanels[SUBPANEL_EAST_INDEX].add(eastBorderPanel, BorderLayout.EAST);
+		scrollPaneBorderPanels[SUBPANEL_EAST_INDEX].add(centerPanel, BorderLayout.CENTER);
+		
 		//Layout
-		scrollPanePanel.add(scrollPaneBorderPanels[0], BorderLayout.NORTH);
-		scrollPanePanel.add(scrollPaneBorderPanels[1], BorderLayout.EAST);
-		scrollPanePanel.add(scrollPaneBorderPanels[2], BorderLayout.SOUTH);
-		scrollPanePanel.add(scrollPaneBorderPanels[3], BorderLayout.WEST);
+		scrollPanePanel.add(scrollPaneBorderPanels[SUBPANEL_NORTH_INDEX], BorderLayout.NORTH);
+		scrollPanePanel.add(scrollPaneBorderPanels[SUBPANEL_EAST_INDEX], BorderLayout.EAST);
+		scrollPanePanel.add(scrollPaneBorderPanels[SUBPANEL_SOUTH_INDEX], BorderLayout.SOUTH);
+		scrollPanePanel.add(scrollPaneBorderPanels[SUBPANEL_WEST_INDEX], BorderLayout.WEST);
 	}
 	
 	/**
@@ -118,18 +191,21 @@ public class StockView extends JPanel {
 			//Init Data-Values
 			for (int row = 0; row < media.length; row++){
 				IDs[row] = media[row].getID();
-				data[row][0] = media[row].getTitle();
-				data[row][1] = media[row].getAuthor();
-				data[row][2] = media[row].getPublisher();
-				data[row][3] = media[row].getEdition();
-				data[row][4] = media[row].getIsbn();
-				data[row][5] = media[row].getCopies();
-				data[row][6] = media[row].getOnStock();
-				data[row][7] = media[row].getKeywords();
+				data[row][COL_TITLE] = media[row].getTitle();
+				data[row][COL_AUTHOR] = media[row].getAuthor();
+				data[row][COL_PUBLISHER] = media[row].getPublisher();
+				data[row][COL_EDITION] = media[row].getEdition();
+				data[row][COL_ISBN] = media[row].getIsbn();
+				data[row][COL_COPIES] = media[row].getCopies();
+				data[row][COL_ONSTOCK] = media[row].getOnStock();
+				data[row][COL_KEYWORDS] = media[row].getKeywords();
 			}
 			if(StockView.this.mode == User.ROLE_LIBRARIAN){
 				for (int row = 0; row < media.length; row++){
-					data[row][8] = new String("löschen"+row);
+//					data[row][COL_ACTIONS] = new String("löschen"+row);
+					JPanel panel = new JPanel();
+					panel.add(new JLabel("tttt"));
+					data[row][COL_ACTIONS] = panel;
 				}
 			}
 		}
@@ -162,7 +238,7 @@ public class StockView extends JPanel {
 
 		@Override
 		public boolean isCellEditable(int row, int col) {
-			if (StockView.this.mode == User.ROLE_LIBRARIAN && col <= 7) {
+			if (mode == User.ROLE_LIBRARIAN && col <= 7) {
 				return true;
 			}
 			return false;
@@ -187,28 +263,28 @@ public class StockView extends JPanel {
 	        MediaHandler mediaHandler = MediaHandler.getInstance();
 			Medium medium = mediaHandler.getMedium(IDs[row]);
 			switch(column){
-				case 0:
+				case COL_TITLE:
 					medium.setTitle(String.valueOf(data));
 					break;
-				case 1:
+				case COL_AUTHOR:
 					medium.setAuthor(String.valueOf(data));
 					break;
-				case 2:
+				case COL_PUBLISHER:
 					medium.setPublisher(String.valueOf(data));
 					break;
-				case 3:
+				case COL_EDITION:
 					medium.setEdition(Integer.parseInt(String.valueOf(data)));
 					break;
-				case 4:
+				case COL_ISBN:
 					medium.setIsbn(String.valueOf(data));
 					break;
-				case 5:
+				case COL_COPIES:
 					medium.setCopies(Integer.parseInt(String.valueOf(data)));
 					break;
-				case 6:
+				case COL_ONSTOCK:
 					medium.setOnStock(Integer.parseInt(String.valueOf(data)));
 					break;
-				case 7:
+				case COL_KEYWORDS:
 					medium.setKeywords(String.valueOf(data));
 					break;
 			}
