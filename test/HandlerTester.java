13f41670cf8a75c9af4d4f08adccbee2127321a8
weisseth <weisseth@dhbw-loerrach.de>
2013-05-22 16:39:56 +0200

    HandlerTest

--- test/HandlerTester.java
+++ test/HandlerTester.java
@@ -2,6 +2,7 @@ package test;
 
 import model.*;
 import controller.*;
+import core.CsvHandler;
 
 public class HandlerTester {
 
@@ -10,38 +11,41 @@ public class HandlerTester {
 	 */
 	public static void main(String[] args) {
 		
+		
+		CsvHandler csvHandler = new CsvHandler("stock");
+//		csvHandler.viewLineById("0");
 		/*
 		 * Reservierungen
 		 */
-		Reservation[] test = ReservationHandler.getInstance().getAllReservations();
-		System.out.println("Index\tLoginName\tMediaID\tRückgabeDatum\tVerlängerungen");
-		for(int i = 0; i < test.length; i++){
-			String output = "";
-			String[] values = test[i].getValuesAsStringArray();
-			for(int k = 0; k < values.length; k++){
-				output += values[k] + "\t";
-			}
-			System.out.println(output);
-		}
-		System.out.println("-----------------------------------------");
-		/*
-		 * User
-		 */
-		UserHandler userHandler = UserHandler.getInstance();
-		User user1 = userHandler.getUser("admin");
-		user1.setPassword("1234");
-		User[] users = userHandler.getAllUsers();
-//		user[0].setLoginName("Admön");
-//		userController.save();
-		for(int i = 0; i < users.length; i++){
-			String output = "";
-			String[] values = users[i].getValuesAsStringArray();
-			for(int k = 0; k < values.length; k++){
-				output += values[k] + "\t";
-			}
-			System.out.println(output);
-		}
-		System.out.println("-----------------------------------------");
+//		Reservation[] test = ReservationHandler.getInstance().getAllReservations();
+//		System.out.println("Index\tLoginName\tMediaID\tRückgabeDatum\tVerlängerungen");
+//		for(int i = 0; i < test.length; i++){
+//			String output = "";
+//			String[] values = test[i].getValuesAsStringArray();
+//			for(int k = 0; k < values.length; k++){
+//				output += values[k] + "\t";
+//			}
+//			System.out.println(output);
+//		}
+//		System.out.println("-----------------------------------------");
+//		/*
+//		 * User
+//		 */
+//		UserHandler userHandler = UserHandler.getInstance();
+//		User user1 = userHandler.getUser("admin");
+//		user1.setPassword("1234");
+//		User[] users = userHandler.getAllUsers();
+////		user[0].setLoginName("Admön");
+////		userController.save();
+//		for(int i = 0; i < users.length; i++){
+//			String output = "";
+//			String[] values = users[i].getValuesAsStringArray();
+//			for(int k = 0; k < values.length; k++){
+//				output += values[k] + "\t";
+//			}
+//			System.out.println(output);
+//		}
+//		System.out.println("-----------------------------------------");
 		
 		MediaHandler mediaHandler = MediaHandler.getInstance();
 		Medium[] media = mediaHandler.getAllMedia();
@@ -53,25 +57,37 @@ public class HandlerTester {
 			}
 			System.out.println(output);
 		}
+
+//		System.out.println("-----------------------------------------");
+//		Medium[] newMedia = mediaHandler.getAllMedia();
+//		for(int i = 0; i < newMedia.length; i++){
+//			String output = "";
+//			String[] values = newMedia[i].getValuesAsStringArray();
+//			for(int k = 0; k < values.length; k++){
+//				output += values[k] + "\t";
+//			}
+//			System.out.println(output);
+//		}
 		
-		System.out.println("-----------------------------------------");
-		Medium[] newMedia = mediaHandler.getAllMedia();
-		for(int i = 0; i < newMedia.length; i++){
-			String output = "";
-			String[] values = newMedia[i].getValuesAsStringArray();
-			for(int k = 0; k < values.length; k++){
-				output += values[k] + "\t";
-			}
-			System.out.println(output);
-		}
+//		newMedia[0].setAuthor("XXXX");
+//		System.out.println(media[0].getAuthor().equals(newMedia[0].getAuthor()));
 		
-		newMedia[0].setAuthor("XXXX");
-		System.out.println(media[0].getAuthor().equals(newMedia[0].getAuthor()));
+		int newID = mediaHandler.getNewID();
+		System.out.println("newID: "+newID);
+		Medium med = mediaHandler.getMedium(newID);
+		med.setAuthor("Frank");
 		
-		newMedia[0].setEdition(4);
-		System.out.println(media[0].getEdition() == newMedia[0].getEdition());
+//		int newID2 = mediaHandler.getNewID();
+//		Medium med2 = mediaHandler.getMedium(newID2);
+//		med2.setAuthor("Max");
+		mediaHandler.save();
 		
 		
+		
+//		newMedia[0].setEdition(4);
+//		System.out.println(media[0].getEdition() == newMedia[0].getEdition());
+		
+		media = mediaHandler.getAllMedia();
 		System.out.println("-----------------------------------------");
 		for(int i = 0; i < media.length; i++){
 			String output = "";
@@ -81,17 +97,7 @@ public class HandlerTester {
 			}
 			System.out.println(output);
 		}
-		
-		System.out.println("-----------------------------------------");
-		for(int i = 0; i < newMedia.length; i++){
-			String output = "";
-			String[] values = newMedia[i].getValuesAsStringArray();
-			for(int k = 0; k < values.length; k++){
-				output += values[k] + "\t";
-			}
-			System.out.println(output);
-		}
-		
+
 	}
 
 }
